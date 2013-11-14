249ffc9dee78c267ecbbc7df5e173947c7e1a61a
rdfricker <rdfricker@nps.edu>
2013-11-13 16:29:32 -0800

    Final update for 11/13

--- server.R
+++ server.R
@@ -26,7 +26,7 @@ shinyServer(function(input,output){
   	if (input$typeDist == "bin") {	
       x <- 0:round(input$num_bin)
       y <- dbinom(x,input$num_bin,input$prob_bin)
-      plot(x,y,type="n",main="",xlab=expression(y),ylab=expression(P(Y==y),ylim=c(0,100)))
+      plot(x,y,type="n",main="",xlab=expression(y),ylab=expression(P(Y==y),ylim=c(0,max(y))))
       lines(c(-2,input$num_bin+2),c(0,0))
       for(i in 1:(input$num_bin+1)){rect(x[i]-0.5,y[i],x[i]+0.5,0,col="gray")}
       
@@ -77,7 +77,7 @@ shinyServer(function(input,output){
       plot(x,y,type="n",main="",xlab=expression(y),ylab=expression(P(Y==y)),ylim=c(0,max(y)))
       lines(c(-2,input$sample_hyp+2),c(0,0))
       for(i in 1:(input$sample_hyp+1)){rect(x[i]-0.5,y[i],x[i]+0.5,0,col="gray")}  
-              
+             
       mu <- input$sample_hyp*input$red_hyp/input$pop_hyp
       sigma <- sqrt((input$sample_hyp*input$red_hyp/input$pop_hyp)*((input$pop_hyp-input$red_hyp)/input$pop_hyp)*((input$pop_hyp-input$sample_hyp)/(input$pop_hyp-1)))    
       if(input$show1SD==TRUE) plot1SD(mu,sigma)
